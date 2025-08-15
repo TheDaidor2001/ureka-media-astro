3df6bedf8bf303ca8f0f6a6f16964a2b8e06f1b4
Daniel Castillo <danielcastillobalboa@gmail.com>
2025-08-15 16:03:09 +0100

    feat: add proyectos section

---
+++ src/data/Proyects.ts
@@ -0,0 +1,58 @@
+import { v4 as uuid } from 'uuid'
+
+export const PROYECTS = [
+    {
+        id: uuid(),
+        tag: 'Producción Audiovisual',
+        title: 'Documental corporativo',
+        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+        images: [
+            'hero-studio.webp'
+        ]
+    },
+    {
+        id: uuid(),
+        tag: 'Producción Audiovisual',
+        title: 'Video promocional',
+        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+        images: [
+            'hero-studio.webp'
+        ]
+    },
+    {
+        id: uuid(),
+        tag: 'Producción Audiovisual',
+        title: 'Spot publicitario',
+        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+        images: [
+            'hero-studio.webp'
+        ]
+    },
+    {
+        id: uuid(),
+        tag: 'Producción Audiovisual',
+        title: 'Video musical',
+        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+        images: [
+            'hero-studio.webp'
+        ]
+    },
+    {
+        id: uuid(),
+        tag: 'Producción Audiovisual',
+        title: 'Cobertura de evento',
+        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+        images: [
+            'hero-studio.webp'
+        ]
+    },
+    {
+        id: uuid(),
+        tag: 'Producción Audiovisual',
+        title: 'Entrevista corporativa',
+        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+        images: [
+            'hero-studio.webp'
+        ]
+    }
+];
